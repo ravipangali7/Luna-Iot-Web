54f0c785d4c6c07bcbe70ad32bbab44d5a1a37e8
Ravi Pangali <lilathapamagar453@gmail.com>
2025-09-06 22:46:24 +0545

    role based

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['app.mylunago.com'],
+    allowedHosts: ['app.mylunago.com', 'system.mylunago.com'],
     proxy: {
+      '/api': {
+        target: 'http://system.mylunago.com:7070',
+        changeOrigin: true,
+        secure: false
+      },
       '/socket.io': {
         target: 'http://38.54.71.218:7070',
         ws: true,
