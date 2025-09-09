95b8157382cf24d792299b68445d31602c435a2c
Ravi Pangali <lilathapamagar453@gmail.com>
2025-09-09 18:54:15 +0545

    CHnages

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,13 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]'
+      }
+    }
+  },
+  publicDir: 'public'
 })
