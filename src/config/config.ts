e800f4f184dccdaa44b5d568d7285d0e732c59a3
Infelo <infelo453@gmail.com>
2025-09-23 16:39:48 +0545

    Chagnes

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ export const API_CONFIG = {
     // Python backend (new function-based APIs)
     BASE_URL: 'https://py.mylunago.com',  // Local Python Django backend
     // BASE_URL: 'http://38.54.71.218:7171/',  // Local Python Django backend
-    SOCKET_URL: 'http://38.54.71.218:6060',
+    SOCKET_URL: 'https://py.mylunago.com',
     
   } as const;
   
